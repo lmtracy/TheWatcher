9f0f461a391a9fa5a509a47e4839d5c75c5e22aa
Lauren Tracy <rabidteddy5@yahoo.com>
2020-02-18 16:33:41 -0500

    First draft
    Connected to OMDB API

---
+++ main.js
@@ -0,0 +1,81 @@
+$(document).ready(() => {
+  $('#searchForm').on('submit', (e) => {
+    let searchText = $('#searchText').val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
+});
+
+function getMovies(searchText){
+  axios.get('http://www.omdbapi.com?apikey=e51d793f&s='+searchText)
+    .then((response) => {
+      console.log(response);
+      let movies = response.data.Search;
+      let output = '';
+      $.each(movies, (index, movie) => {
+        output += `
+          <div class="col-md-3">
+            <div class="well text-center">
+              <img src="${movie.Poster}">
+              <h5>${movie.Title}</h5>
+              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+            </div>
+          </div>
+        `;
+      });
+
+      $('#movies').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function movieSelected(id){
+  sessionStorage.setItem('movieId', id);
+  window.location = 'movie.html';
+  return false;
+}
+
+function getMovie(){
+  let movieId = sessionStorage.getItem('movieId');
+
+  axios.get('http://www.omdbapi.com?apikey=e51d793f&i='+movieId)
+    .then((response) => {
+      console.log(response);
+      let movie = response.data;
+
+      let output =`
+        <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.Title}</h2>
+            <ul class="list-group">
+              <li class="list-group-item list-group-item-dark"><strong>Year:</strong> ${movie.Year}</li>
+              <li class="list-group-item list-group-item-dark"><strong>Genre:</strong> ${movie.Genre}</li>
+              <li class="list-group-item list-group-item-dark"><strong>Director:</strong> ${movie.Director}</li>
+              <li class="list-group-item list-group-item-dark"><strong>Writer:</strong> ${movie.Writer}</li>
+              <li class="list-group-item list-group-item-dark"><strong>Rating:</strong> ${movie.Rated}</li>
+              <li class="list-group-item list-group-item-dark"><strong>Runtime:</strong> ${movie.Runtime}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Plot</h3>
+            ${movie.Plot}
+            <hr>
+            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="index.html" class="btn btn-default">Go Back To Search</a>
+          </div>
+        </div>
+      `;
+
+      $('#movie').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
